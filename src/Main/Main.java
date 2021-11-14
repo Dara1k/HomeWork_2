443dfa9b9878b882fe050f601692acbe2a8d2664
Dara1k <dencik@bk.ru>
2021-11-14 09:54:39 +0300

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+                double a = 5.12;
+                byte max = Byte.MAX_VALUE;
+                System.out.println (a);
+                System.out.println(max);
+
+            }
+
+
+
+    }
+
